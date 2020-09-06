68d57547b23240daa1626262bfa44da5f6f2a113
kawaitoi <kawaitoi123@gail.com>
2020-09-06 12:38:51 +0900

    feat:firebase連携

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,9 +22,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule ,
+    AngularFireStorageModule ,
+    AngularFireFunctionsModule,
+
   ],
-  providers: [],
+  providers: [
+    { provide: REGION, useValue: 'asia-northeast1' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAo4bfNjhT9ogvEoY80SWdCS6egaufYz5c",
+  authDomain: "gitpet-test-18984.firebaseapp.com",
+  databaseURL: "https://gitpet-test-18984.firebaseio.com",
+  projectId: "gitpet-test-18984",
+  storageBucket: "gitpet-test-18984.appspot.com",
+  messagingSenderId: "1075248318689",
+  appId: "1:1075248318689:web:e0798cca65fd5bfcfacfa0",
+  measurementId: "G-7ZSH3VFC8X"
+  }
 };
 
 /*
